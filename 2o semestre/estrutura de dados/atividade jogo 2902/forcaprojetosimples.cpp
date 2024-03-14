70d0a36ca979487dcc841cad8f448a725823c47f
Rafael Kazuhito Vidal Beltrão <115881851+rkazuhito@users.noreply.github.com>
2024-03-14 19:03:32 -0300

    forca update

--- 2o semestre/estrutura de dados/atividade jogo 2902/forcaprojetosimples.cpp
+++ 2o semestre/estrutura de dados/atividade jogo 2902/forcaprojetosimples.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
-#include <stdlib.h>
+#include <stdlib.h> //pro system cls
 
 using namespace std;
 
 int main(){
 	char palavra[30],letra[1],secreta[30];
 	int tam,i,chances,acertos;
-	bool acerto=false;
+	bool acerto=false; //verifica se a letra digitada esta certa ou errada
 	
 	chances=6;
 	tam=0;
 	i=0;
-	acertos=0;
+	acertos=0; //conta a qtd de acertos
 	
 	cout << "Digite a palavra da forca: ";
 	cin >> palavra;
