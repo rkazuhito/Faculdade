68b6ede1cd762cad48e8a8cfeb2dbbbd91df752c
Rafael Kazuhito Vidal Beltrão <115881851+rkazuhito@users.noreply.github.com>
2024-03-15 18:00:38 -0300

    forca finalizado

--- 2o semestre/estrutura de dados/atividade jogo 2902/forcaprojetosimples.cpp
+++ 2o semestre/estrutura de dados/atividade jogo 2902/forcaprojetosimples.cpp
@@ -17,10 +17,42 @@ int main(){
 	cin >> palavra;
 	system("cls"); //comando para limpar a tela
 	
-	while(palavra[i]!= '\0'){ //\0 é a posição do enter, ou seja, vai percorrer a string até chegar no fim da palavra
+	while(palavra[i] != '\0'){ //\0 é a posição do enter, ou seja, vai percorrer a string até chegar no fim da palavra
 		i++;
 		tam++;
 	}
 	
+	for(i=0;i<30;i++){
+		secreta[i] = '_';
+	}
+	
+	while((chances>0)&&(acertos<tam)){ //rotina do jogo será toda nesse loop
+		cout << "Canches restantes: "<< chances << "\n";
+		cout << "Palavra secreta: ";
+		for(i=0; i<tam; i++){
+			cout <<secreta[i];
+		}
+		cout << "\n\n Digite uma letra: ";
+		cin >> letra[0];
+		for (i=0;i<tam;i++){
+			if(palavra[i]==letra[0]){
+				acerto=true;
+				secreta[i]=palavra[i];
+				acertos++;
+			}
+		}
+		if(!acerto){
+			chances--;
+		}
+		acerto=false; //refaz a verificação do acerto
+		system ("cls");
+	}
+	
+	if(acertos==tam){
+		cout << "Você venceu, parabens!";
+	}else{
+		cout <<"que pena, perdeu";
+	}
+	system("pause");
 	return 0;
 }
