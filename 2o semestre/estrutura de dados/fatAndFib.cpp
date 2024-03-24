c64a4a625a0b61eebc7b2ecda9f7d9944aba8333
Rafael Kazuhito Vidal Beltrão <115881851+rkazuhito@users.noreply.github.com>
2024-03-24 17:43:51 -0300

    Create fatAndFib.cpp

---
+++ 2o semestre/estrutura de dados/fatAndFib.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+int fat(int n);
+int fib(int n);
+
+int main(){
+    int val, res;
+    cout <<"informe o valor";
+    cin >> val;
+    res=fat(val);
+    cout << val <<"!= " << res <<"\n";
+    cout<< "fibonacci = ";
+    for(int i=0;i<val;i++){
+        cout <<fib(i+1)<<" ";
+    }
+    return 0;
+}
+
+int fat(int n){
+    if (n==0){
+        return 1;
+    }
+    return n*fat(n-1);
+}
+int fib(int n){
+    if (n==1||n==2){
+        return 1;
+    }
+    else{
+        return fib(n-1)+fib(n-2);
+    }
+}
